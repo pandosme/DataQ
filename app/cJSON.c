c3171e6faa92177bc4e1397e472d1d1dcf8c09c0
Fred Juhlin <fred.juhlin@gmail.com>
2025-02-24 14:41:35 +0100

    1.3.0 Beta

--- app/cJSON.c
+++ app/cJSON.c
@@ -576,19 +576,15 @@ static cJSON_bool print_number(const cJSON * const item, printbuffer * const out
     }
     else
     {
-		if( d > 100000)
-			length = sprintf((char*)number_buffer, "%.0f", d);
-		else
-			length = sprintf((char*)number_buffer, "%.1f", d);
         /* Try 15 decimal places of precision to avoid nonsignificant nonzero digits */
-//        length = sprintf((char*)number_buffer, "%1.15g", d);
+        length = sprintf((char*)number_buffer, "%1.15g", d);
 
         /* Check whether the original double can be recovered */
-//        if ((sscanf((char*)number_buffer, "%lg", &test) != 1) || !compare_double((double)test, d))
-//        {
+        if ((sscanf((char*)number_buffer, "%lg", &test) != 1) || !compare_double((double)test, d))
+        {
             /* If not, print with 17 decimal places of precision */
-//            length = sprintf((char*)number_buffer, "%1.17g", d);
-//        }
+            length = sprintf((char*)number_buffer, "%1.17g", d);
+        }
     }
 
     /* sprintf failed or buffer overrun occurred */
